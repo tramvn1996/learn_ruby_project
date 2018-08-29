ff7355ad89b7fdecf46f0ce9d6551edc7c5190ac
tramvn1996 <tramvn1996@gmail.com>
2018-08-28 23:11:23 -0400

    Merge branch 'master' of https://github.com/tramvn1996/learn_ruby_project

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -6,6 +6,8 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get root" do
     get static_pages_home_url
     assert_response  :success
+    assert_select "title", "Ruby on Rails Tutorial Sample App"
+
   end
 
   test "should get home" do
